1b01b7cd17f86f125a3d9fedde7734ce474ce2bd
harryzhu <harry.chuh@gmail.com>
2022-12-11 23:07:39 +0800

    -

--- bar64.go
+++ bar64.go
@@ -6,34 +6,34 @@ import (
 	"sync"
 )
 
-type Bar struct {
+type Bar64 struct {
 	Counter    int
 	CounterMax int
 	Max64      int64
 	Current64  int64
 }
 
-var WGpbar sync.WaitGroup
-var pbar Bar
+var wg64 sync.WaitGroup
+var bar64 Bar64
 
-func NewBar(n int64) *Bar {
-	WGpbar = sync.WaitGroup{}
-	WGpbar.Add(1)
+func NewBar64(n int64) *Bar64 {
+	wg64 = sync.WaitGroup{}
+	wg64.Add(1)
 
-	return &Bar{
+	bar64 = &Bar{
 		Counter:   0,
 		Max64:     n,
 		Current64: 0,
 	}
-
+	return bar64
 }
 
-func (b *Bar) WithMax64(n int64) *Bar {
+func (b *Bar64) WithMax64(n int64) *Bar64 {
 	b.Max64 = n
 	return b
 }
 
-func (b *Bar) Add64(n int64) error {
+func (b *Bar64) Add64(n int64) error {
 	b.Current64 = b.Current64 + n
 	b.Counter = b.Counter + 1
 
@@ -54,18 +54,18 @@ func (b *Bar) Add64(n int64) error {
 	return nil
 }
 
-func (b *Bar) Write(bt []byte) (n int, err error) {
+func (b *Bar64) Write(bt []byte) (n int, err error) {
 	n = len(bt)
 	b.Add64(int64(n))
 	return
 }
 
-func (b *Bar) Read(bt []byte) (n int, err error) {
+func (b *Bar64) Read(bt []byte) (n int, err error) {
 	n = len(bt)
 	b.Add64(int64(n))
 	return
 }
 
-func (b *Bar) Finish() {
-	WGpbar.Done()
+func (b *Bar64) Finish() {
+	wg64.Done()
 }
